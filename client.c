f68328b17bbeceb65c32e4b7d1a769d71cb16297
Dana Betsina <bettkina@gmail.com>
2024-11-08 13:04:59 +0100

    - pingpong - print wiadomosci-komendy od serwera - rownolegle watkowanie

--- client.c
+++ client.c
@@ -16,8 +16,13 @@
 #define HEADER_SIZE 10
 #define INET_ADDRSTRLEN 16 
 
+#define PING_INTERVAL_MS 410
+#define PONG_TIMEOUT_MS 100
+
 // Definicja MUTEX ;)
 pthread_mutex_t dictionary_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
 int sockfd;
 int pipefd[2];
 
@@ -126,7 +131,8 @@ void add_server(const char *ip, int port)
     new_server->flag = true;
 
     server_count++;
-    printf("Added server to dictionary: ID: %s, IP: %s, Port: %d, Flag: %s\n",
+    printf("%d\n", server_count);
+    printf("Dodano serwer o wartościach ID: %s, IP: %s, Port: %d, Flaga: %s\n",
            new_server->id, new_server->ip, new_server->port,
            new_server->flag ? "true" : "false");
 
@@ -148,6 +154,7 @@ void worker_process() {
     }
 }
 
+struct timespec sent_times[MAX_SERVERS];
 // Funkcja sprawdzająca komunikaty od serwerów
 void check_servers(){
     // printf("Czekam!");
@@ -160,7 +167,8 @@ void check_servers(){
     while (1)
     {
         // Odbieranie wiadomości z gniazda
-        int received_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&addr, &addr_len);
+        int received_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, 
+        (struct sockaddr *)&addr, &addr_len);
         if (received_len < 0)
         {
             perror("Błąd podczas odbierania danych");
@@ -173,6 +181,9 @@ void check_servers(){
         inet_ntop(AF_INET, &addr.sin_addr, server_ip, INET_ADDRSTRLEN);
         printf("Otrzymano wiadomość od serwera IP: %s, Wiadomość: %s\n", server_ip, buffer);
 
+        struct timespec received_time;
+        clock_gettime(CLOCK_REALTIME, &received_time);
+
         // Parsowanie nagłówka i obsługa wiadomości
         char msg_type[HEADER_SIZE] = {0};
         sscanf(buffer, "%9s", msg_type);
@@ -183,6 +194,28 @@ void check_servers(){
             write(pipefd[1], server_ip, INET_ADDRSTRLEN);
             int port = ntohs(addr.sin_port);
             write(pipefd[1], &port, sizeof(int));
+        } else if (strcmp(msg_type, CMD_MSG) == 0) {
+            // odpowiedz na komende od serwera
+            int server_index = -1;
+            for (int i = 0; i < server_count; i++) {
+                if (strcmp(dictionary[i].ip, server_ip) == 0 && dictionary[i].port == ntohs(addr.sin_port)) {
+                    server_index = i;
+                    break;
+                }
+            }
+            
+
+            // liczymy delay po wyslaniu komendy do serwera
+            long ms_delay = (received_time.tv_sec - sent_times[server_index].tv_sec) * 1000 +
+                            (received_time.tv_nsec - sent_times[server_index].tv_nsec) / 1000000;
+
+            char time_str[100];
+            struct tm *tm_info = localtime(&received_time.tv_sec);
+            strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", tm_info);
+
+            
+            printf("[%s] Otrzymano wiasomosc od serwera %s:%d po czasie %ld ms: %s\n",
+                   time_str, server_ip, ntohs(addr.sin_port), ms_delay, buffer + HEADER_SIZE);
         } 
         // else {
         //     printf("Received non-HELLO message: '%s'\n", msg_type);
@@ -208,22 +241,27 @@ void get_active_ids(void) {
 Server get_random_active_server(void) {
     // Tablica do przechowywania indeksów serwerów z aktywną flagą
     int active_servers[MAX_SERVERS];
-    int active_count = 0;
+    int active_count_p = 0;
+    printf("%d\n", server_count);
     // Zbieramy serwery, których flaga jest ustawiona na true
     for (int i = 0; i < server_count; i++) {
         if (dictionary[i].flag) {
-            active_servers[active_count++] = i;
+            printf("Check#1\n");
+            active_count_p++;
+            active_servers[active_count_p] = i;
         }
+        printf("Check#2\n");
     }
     // Jeśli nie ma aktywnych serwerów, zwróć pustą strukturę lub odpowiednią wartość
-    if (active_count == 0) {
+    if (active_count_p == 0) {
         printf("Brak aktywnych serwerów.\n");
         Server empty_server = {0};  // Tworzymy pusty serwer
         return empty_server;  // Zwracamy pustą strukturę
     }
     // Losujemy jeden serwer z aktywnych serwerów
     srand(time(NULL));  // Inicjalizowanie generatora liczb losowych
-    int random_index = rand() % active_count;
+    int random_index = rand() % active_count_p;
+
     // Zwracamy losowy serwer i przy okazji printuje go 
     printf("Wylosowany aktywny serwer: %s, IP: %s, Port: %d\n", 
     dictionary[active_servers[random_index]].id, dictionary[active_servers[random_index]].ip, dictionary[active_servers[random_index]].port);
@@ -255,12 +293,12 @@ void *send_random_command(void *arg) {
         // Blokowanie mutexu, aby uzyskać dostęp do listy serwerów
         pthread_mutex_lock(&dictionary_mutex);
 
+        // printf("%d\n", server_count);
         // Sprawdzenie, czy są jakieś aktywne serwery
         if (server_count == 0) {
-            printf("Brak aktywnych serwerów do wysłania wiadomości.\n");
-            pthread_mutex_unlock(&dictionary_mutex);
-            continue;
+            printf("Waiting for active servers...\n");
         }
+        pthread_mutex_unlock(&dictionary_mutex);
 
         // Pobranie losowego aktywnego serwera
         Server random_server = get_random_active_server();
@@ -283,7 +321,13 @@ void *send_random_command(void *arg) {
         // Generowanie losowej wiadomości o długości 23 znaków
         char message[24];
         snprintf(message, sizeof(message), "%s %s", CMD_MSG, generate_random_string());
-
+        // fix czasu wyslania
+        for (int i = 0; i < server_count; i++) {
+            if (strcmp(dictionary[i].id, random_server.id) == 0) {
+                clock_gettime(CLOCK_REALTIME, &sent_times[i]);
+                break;
+            }
+        }
         // Wysłanie wiadomości do serwera
         if (sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&server_addr, addr_len) < 0) {
             perror("Błąd podczas wysyłania wiadomości CMD");
@@ -294,7 +338,71 @@ void *send_random_command(void *arg) {
     return NULL;
 }
 
+void *ping_servers(void *arg) {
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+    char message[] = "Ping";
+    char buffer[BUFFER_SIZE];
+    struct timespec timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_nsec = PONG_TIMEOUT_MS * 1000000L; // konwertujemy do ns
+
+    while (1) {
+        pthread_mutex_lock(&dictionary_mutex);
+        
+        for (int i = 0; i < server_count; i++) {
+            if (dictionary[i].flag) {  
+                
+                memset(&server_addr, 0, sizeof(server_addr));
+                server_addr.sin_family = AF_INET;
+                server_addr.sin_port = htons(dictionary[i].port);
+                inet_pton(AF_INET, dictionary[i].ip, &server_addr.sin_addr);
+
+                // Wysylamy Ping
+                if (sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&server_addr, addr_len) < 0) {
+                    perror("Błąd przy wysłaniu Ping");
+                    continue;
+                }
+                
+                // Czekamy przed dostaniem Pong
+                fd_set fds;
+                FD_ZERO(&fds);
+                FD_SET(sockfd, &fds);
+
+                int retval = pselect(sockfd + 1, &fds, NULL, NULL, &timeout, NULL);
+                if (retval == -1) {
+                    perror("Błąd podczas oczekiwania wiadomości Pong");
+                    continue;
+                } else if (retval > 0 && FD_ISSET(sockfd, &fds)) {
+                    // Otrzymujemy odpowiedz
+                    int received_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
+                    if (received_len > 0) {
+                        buffer[received_len] = '\0';
+                        if (strcmp(buffer, "Pong") == 0) {
+                            printf("Serwer %s jest aktywny (ID: %s)\n", dictionary[i].ip, dictionary[i].id);
+                            continue;
+                        }
+                    }
+                }
+
+                // Jezeli ni ma odpowiedzi, zmieniamy flage
+                printf("Serwer %s (ID: %s) nie odpowiada. Zmieniam flagę.\n", dictionary[i].ip, dictionary[i].id);
+                
+                change_flag(dictionary[i].id, false);
+            }
+        }
+        
+        pthread_mutex_unlock(&dictionary_mutex);
+        
+        // Сzekamy 410ms przed następnym pingowaniem
+        usleep(PING_INTERVAL_MS * 1000); // do ms
+    }
+
+    return NULL;
+}
 int main(){
+
+    
 
     if (signal(SIGINT, handle_exit) == SIG_ERR) {
         perror("Error setting SIGINT handler\n");
@@ -332,7 +440,7 @@ int main(){
         return EXIT_FAILURE;
     }
 
-    printf("Klient uruchomiony, oczekiwanie na wiadomości HELLO...\n");
+    printf("Klient uruchomiony\n");
 
     // Create a pipe for IPC !!! NOWEEEEEEEEEEEEEEEEEEEEEEEEEEE
     if (pipe(pipefd) == -1) {
@@ -350,29 +458,23 @@ int main(){
         // Child process - worker
         worker_process();
     } else {
-        // Proces nadrzędny - sprawdzanie wiadomości od serwerów
-        printf("Klient uruchomiony, oczekiwanie na wiadomości HELLO...\n");
-
-        // Uruchomienie wątku do wysyłania losowych wiadomości CMD
-        pthread_t cmd_thread;
-        if (pthread_create(&cmd_thread, NULL, send_random_command, NULL) != 0) {
-            perror("Błąd podczas tworzenia wątku dla send_random_command");
+        pthread_t send_cmd_thread, ping_thread;
+        if (pthread_create(&send_cmd_thread, NULL, send_random_command, NULL) != 0) {
+            perror("Błąd przy tworzeniu wątku dla send_random_command");
             close(sockfd);
             return EXIT_FAILURE;
         }
 
-        // Uruchomienie funkcji `check_servers` w procesie nadrzędnym
-        check_servers(sockfd);
-
-        // Oczekiwanie na zakończenie wątku `cmd_thread`
-        pthread_join(cmd_thread, NULL);
-        
-        // Zamknięcie potoku i gniazda po zakończeniu
-        close(pipefd[0]);
-        close(pipefd[1]);
-        close(sockfd);
+        if (pthread_create(&ping_thread, NULL, ping_servers, NULL) != 0) {
+            perror("Błąd przy tworzeniu wątku dla ping_servers");
+            close(sockfd);
+            return EXIT_FAILURE;
+        }
+        check_servers();
+        pthread_join(send_cmd_thread, NULL);
+        pthread_join(ping_thread, NULL);
     }
-
+}
     // Chyba jakoś tak by to wyglądało(?), jeszcze dodanie losowego ciągu
     // while (1) {
     //     get_active_ids();  // Wypisujemy dostępne serwery
@@ -396,4 +498,4 @@ int main(){
     //     printf("Sent CMD message: '%s' to server %s:%d\n", cmd_message, ip, port);
     // }
     // !!!    
-}
+
